36f47d261122be2c7cdfff8376b5be36e6d969cc
Kate <bastowkate@gmail.com>
2022-12-12 13:06:54 +1100

    Update server.js

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ const app = express()
 app.use(express.static('public'));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use('*', cors())
 app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 }
   }))
